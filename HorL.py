11a4e36dd0009179320a870ff76d27ea7fbb8ca5
uvjour <uvjour@gmail.com>
2022-06-14 13:24:52 -0400

    update methods

--- HorL.py
+++ HorL.py
@@ -31,5 +31,26 @@ while True:
     gameDeckList = shuffler(deckList)
     currentDeckList = getCard(gameDeckList)
     currentrank = currentDeckList['rank']
-    
+    currentsuit = currentDeckList['suit']
+    currentvalue = currentDeckList['value']
+    print(f'Starting card is {currentrank} of {currentsuit}')
+    print('')
+
+    for cardnumber in range(numoftries):
+        answer = input(f'Will the next card be higher or lower than {currentrank} of {currentsuit}? Enter h for higher or l for lower!')
+        answer = answer.lower()
+        nextCard = getCard(gameDeckList)
+        nextrank = nextCard['rank']
+        nextsuit = nextCard['suit']
+        nextvalue = nextCard['value']
+        print(f'And the next card is {nextrank} of {nextsuit}')
+        if answer == 'h':
+            if nextvalue > currentvalue:
+                print('Correct! It was higher')
+                score += 20
+            else:
+                print('Wrong! Unfortunately that was not right')
+                score -= 15
+
+        elif answer
 
